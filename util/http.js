f10945c9fdb7699768aad8392b7554a18e5e346e
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-04-23 18:50:50 +0530

    GET and POST request handling

--- util/http.js
+++ util/http.js
@@ -1,9 +1,25 @@
 import axios from 'axios';
 
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
 export function storeExpense(expenseData) {
-  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
-  axios.post(
-    `${apiUrl}/expenses.json`,
-    expenseData
-  );
+  axios.post(`${apiUrl}/expenses.json`, expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(`${apiUrl}/expenses.json`);
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
